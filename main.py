5e812f7d2ec004251dfec1898f7f60abb39fc915
bdidk235 <53656028+bdidk235@users.noreply.github.com>
2022-02-21 14:44:05 +0200

    Why?

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ token = str(os.environ.get('bot_token'))
     description="Frequently Asked Questions!",
     options=[
         disnake.Option("question", "What is your question?", disnake.OptionType.string, True, choices = [
-            disnake.OptionChoice("What is this?", "faq"),
-            disnake.OptionChoice("Who are you?", "bot"),
+            disnake.OptionChoice("Why?", "why"),
+            disnake.OptionChoice("What are you?", "bot"),
             disnake.OptionChoice("How are you made?", "bot_creation"),
             disnake.OptionChoice("What Admins do?", "admins"),
             disnake.OptionChoice("Yoy?", "yoy"),
@@ -21,9 +21,9 @@ token = str(os.environ.get('bot_token'))
 )
 async def faq(inter, question: str):
     answers = {
+        "why": "Why not?",
         "bot": "I'm a bot made for <@628260543588859904> because he's cool by <@622670618822967296> for the Generic RPG Game Server.",
         "bot_creation": "I'm made using python with disnake.",
-        "faq": "I'm trying to answer your questions!",
         "admins": "Admins can do a lot of thing, and moderate the game they have access to admin commands and some other stuff.",
         "yoy": "yoy :smiley: :smiley: :smiley:"
     }
