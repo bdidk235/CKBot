e392267bdef7cd8f573829693677f9a00a27ca11
bdidk235 <53656028+bdidk235@users.noreply.github.com>
2022-07-14 16:37:14 +0300

    Now I know, I know... never

--- extras.py
+++ extras.py
@@ -1,6 +1,26 @@
+import main
 import random
 from youtubesearchpython import SearchVideos
 from disnake import *
+from disnake.ext import commands
+
+async def respond(bot: commands.Bot, re: Interaction, author, question: str, private: bool = False):
+    speech_type = main.all_speech_types
+    if private:
+        speech_type = main.speech_types
+    response = None
+    if question.lower().find("gay") != -1:
+        response = "<:chillbob:964329423232991282>"
+    elif question.lower().find("i run") != -1:
+        response = "START RUNNING NOW!"
+    elif question.lower().find("i die") != -1 or ((question.lower().find("not like") != -1 or question.lower().find("dislike") != -1 or question.lower().find("hate") != -1) and question.lower().find("yoy") != -1) or (question.lower().find("nsfw") != -1 and not (question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1)):
+        response = "No"
+    elif question.lower().find("not die") != -1 or question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1 or question.lower().find("yoy") != -1:
+        response = "Yes"
+    else:
+        response = speech_type[hash(question.lower().replace(f"<@!{bot.user.id}>", "").replace(" ", "")) % len(speech_type)].replace("$username", f"<@!{author.id}>")
+    print(f"{author}: {question} (DM: {private})")
+    await re.send(response)
 
 def unique_random_unicode(length):
     random_ints = random.sample(range(0x07FF), length)
@@ -9,48 +29,3 @@ def unique_random_unicode(length):
 
 def youtube_search(search, max_results:int = 10):
     return [video for video in SearchVideos(search, mode = "dict", max_results = max_results).result()['search_result']]
-
-class Element:
-    def __init__(self, *, header, short_desc=None, long_desc=None, elements=None):
-        self._parent = None
-        self.header = header
-        self.short_desc = short_desc or ""
-        self.long_desc = long_desc or ""
-        self.elements = elements or []
-        self.current = 0
-        # Set parents
-        for elem in self.elements:
-            elem._parent = self
-    
-    @property
-    def parent(self):
-        return self if self._parent is None else self._parent
-
-    @property
-    def element(self):
-        if len(self.elements) == 0:
-            return self
-        return self.elements[self.current]
-
-    def next_elem(self):
-        if len(self.elements) > 0:
-            self.current = (self.current + 1) % len(self.elements)
-    
-    def prev_elem(self):
-        if len(self.elements) > 0:
-            self.current = (self.current - 1) % len(self.elements)
-    
-    def display_elements(self, sep="\n"):
-        table = ""
-        for i, elem in enumerate(self.elements):
-            table = f"{table}{sep}{elem.display(1, i == self.current)}"
-        return table[len(sep):]
-
-    def display(self, depth=0, highlight=False):
-        header = f"**{self.header}**" if not highlight else f"> **{self.header}**"
-        content = self.long_desc if depth == 0 else self.short_desc
-        content = f"{header}\n{content}"
-        # Also display children
-        if len(self.elements) > 0 and depth < 1:
-            content = f"{content}\n{self.display_elements()}"
-        return content.strip()
--- main.py
+++ main.py
@@ -12,70 +12,77 @@ from disnake.enums import *
 from disnake.ext import commands
 import extras
 
+answers = {
+    "bot": "I'm a bot made for Creatorkill because he's cool by bdidk235 mainly for the Generic RPG Server.",
+    "bot_creation": "I'm made using Python with Disnake, You can also check out the [Source Code](https://github.com/bdidk235/CKBot) for this Bot.",
+    "bdidk235": "bdidk235 is the developer of CKBot and also develops for the experience and other projects.",
+    "creatorkill": "Creatorkill is a guy who does stuff and my name is based on his username because he's too cool to ignore.",
+    "banana": "In Creatorkill's Basement.",
+    "private": "You can use message me and I will still work with the commands.",
+    "stealing": "Recently We have seen someone steal **air**, We will make sure to find who they are.",
+    "like_me": "I like like you 🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫",
+    "stay_away": "Stay away from me!",
+    "secret": "Don't let anyone know, Keep it a secret.",
+}
+
+faq_choices = [
+    OptionChoice("What are you?", "bot"),
+    OptionChoice("How are you made?", "bot_creation"),
+    OptionChoice("Who is bdidk235?", "bdidk235"),
+    OptionChoice("Who is Creatorkill?", "creatorkill"),
+    OptionChoice("Where is the Banana?", "banana"),
+    OptionChoice("Are you just an FAQ Bot?", "faq"),
+    OptionChoice("Can I use you privately?", "private"),
+    OptionChoice("Is There a Stealing Problem?", "stealing"),
+    OptionChoice("Do you like me?", "like_me"),
+    OptionChoice("I wanna do something bad!", "stay_away"),
+    OptionChoice("I have a secret but I really wanna tell it!", "secret"),
+]
+
+speech_types = [
+    "Yes",
+    "No",
+    "Maybe",
+    "I'm not smart",
+    "What you think you are?",
+    "I met a person before!",
+    "NO, YOU THATS COMPLETELY WRONG!",
+    "You are an idiot!",
+    "no u",
+    "I don't steal, I ask for it!",
+    "I'm stupid",
+    "THIS IS SO IRONIC",
+    "START RUNNING NOW!",
+    "Ok.",
+    "Cancel it.",
+    "You're Cancelled!",
+]
+
+public_speech_types = [
+    "WHERE IS $username, I CANT FIND THEM!",
+    "$username PLEASE STOP!",
+]
+
+all_speech_types = speech_types + public_speech_types
+
+jumpscares = [
+    "https://tenor.com/view/dog-dog-jumpscare-jumpscare-jumpscare-gif-the-dog-gif-23747153",
+    "https://tenor.com/view/sogga-big-floppa-caracal-floppa-big-soggus-gif-23364768",
+    "https://tenor.com/view/markiplier-jumpscare-punch-fnaf-gif-23353403",
+    "https://tenor.com/view/gaster-wd-gaster-undertale-undertale-gaster-gaster-jumpscare-gif-24147829",
+    "https://tenor.com/view/jump-scare-gif-18504612",
+    "https://tenor.com/view/the-rock-the-rock-jumpscare-the-rock-sussy-gif-24038148",
+    "https://tenor.com/view/omori-omori-sunny-sunny-omori-jumpscare-gif-24352720",
+    "https://tenor.com/view/among-us-amogus-jumpscare-jumpscare-gif-among-us-sus-gif-24082720",
+]
+
 def main(token):
-    bot = commands.Bot(command_prefix = commands.when_mentioned, sync_commands = True)
-
-    answers = {
-        "bot": "I'm a bot made for Creatorkill because he's cool by bdidk235 mainly for the Generic RPG Server.",
-        "bot_creation": "I'm made using Python with Disnake, You can also check out the [Source Code](https://github.com/bdidk235/CKBot) for this Bot.",
-        "bdidk235": "bdidk235 is the developer of CKBot and also develops for the experience and other projects.",
-        "creatorkill": "Creatorkill is a guy who does stuff and my name is based on his username because he's too cool to ignore.",
-        "banana": "In Creatorkill's Basement.",
-        "private": "You can use message me and I will still work with the commands.",
-        "stealing": "Recently We have seen someone steal **air**, We will make sure to find who they are.",
-        "like_me": "I like like you 🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫🤫",
-        "stay_away": "Stay away from me!",
-        "secret": "Don't let anyone know, Keep it a secret.",
-    }
-
-    speach_types = [
-        "Yes",
-        "No",
-        "Maybe",
-        "I'm not smart",
-        "What you think you are?",
-        "I met a person before!",
-        "NO, YOU THATS COMPLETELY WRONG!",
-        "WHERE IS $username, I CANT FIND THEM!",
-        "You are an idiot!",
-        "$username PLEASE STOP!",
-        "no u",
-        "I don't steal, I ask for it!",
-        "I'm stupid",
-        "THIS IS SO IRONIC",
-        "START RUNNING NOW!",
-        "Ok.",
-        "Cancel it.",
-        "You're Cancelled!",
-    ]
-
-    jumpscares = [
-        "https://tenor.com/view/dog-dog-jumpscare-jumpscare-jumpscare-gif-the-dog-gif-23747153",
-        "https://tenor.com/view/sogga-big-floppa-caracal-floppa-big-soggus-gif-23364768",
-        "https://tenor.com/view/markiplier-jumpscare-punch-fnaf-gif-23353403",
-        "https://tenor.com/view/gaster-wd-gaster-undertale-undertale-gaster-gaster-jumpscare-gif-24147829",
-        "https://tenor.com/view/jump-scare-gif-18504612",
-        "https://tenor.com/view/the-rock-the-rock-jumpscare-the-rock-sussy-gif-24038148",
-        "https://tenor.com/view/omori-omori-sunny-sunny-omori-jumpscare-gif-24352720",
-        "https://tenor.com/view/among-us-amogus-jumpscare-jumpscare-gif-among-us-sus-gif-24082720",
-    ]
+    bot = commands.Bot(sync_commands = True)
 
     @bot.slash_command(
         description = "Frequently Asked questions.",
         options = [
-            Option("question", "What is your question?", OptionType.string, True, choices = [
-                OptionChoice("What are you?", "bot"),
-                OptionChoice("How are you made?", "bot_creation"),
-                OptionChoice("Who is bdidk235?", "bdidk235"),
-                OptionChoice("Who is Creatorkill?", "creatorkill"),
-                OptionChoice("Where is the Banana?", "banana"),
-                OptionChoice("Are you just an FAQ Bot?", "faq"),
-                OptionChoice("Can I use you privately?", "private"),
-                OptionChoice("Is There a Stealing Problem?", "stealing"),
-                OptionChoice("Do you like me?", "like_me"),
-                OptionChoice("I wanna do something bad!", "stay_away"),
-                OptionChoice("I have a secret but I really wanna tell it!", "secret"),
-            ])
+            Option("question", "What is your question?", OptionType.string, True, choices = faq_choices)
         ]
     )
     async def faq(
@@ -103,6 +110,32 @@ def main(token):
         await inter.send("yoy <:yoy1:943929050097938453><:yoy2:943929050093748255>")
 
     @bot.slash_command(
+        description = "Helps with Research.",
+        options = [
+            Option("search", "Search", OptionType.string, True),
+            Option("amount", "Max Amount", OptionType.integer)
+        ]
+    )
+    async def research(
+        inter: CommandInteraction,
+        search: str,
+        amount: int = 10
+    ):
+        await inter.send("Searching...")
+        try:
+            searches = googlesearch.search(search, min(amount, 25), advanced = True)
+            found_searches = ""
+            for index, gsearch in enumerate(searches):
+                title = gsearch.title
+                link = gsearch.url
+                found_searches += f"{index + 1}: [{title}]({link})\n"
+            embed = Embed(title = f"Results for {search}:", description = found_searches).set_footer(text = "Provided by Google")
+            await inter.edit_original_message(content = None, embed = embed)
+        except Exception:
+            await inter.edit_original_message(content = "Found Nothing, Please Try Again!")
+            traceback.print_exc()
+
+    @bot.slash_command(
         description = "Shows youtube videos you've searched for.",
         options = [
             Option("search", "Video Search", OptionType.string, True),
@@ -114,6 +147,7 @@ def main(token):
         search:str,
         amount:int = 10
     ):
+        await inter.send("Searching...")
         try:
             videos = extras.youtube_search(search, min(amount, 50))
             found_videos = ""
@@ -121,9 +155,10 @@ def main(token):
                 title = video["title"]
                 link = video["link"]
                 found_videos += f"{index + 1}: [{title}]({link})\n"
-            embed = Embed(title = f"Results for {search}:", description = found_videos)
-            await inter.send(embed = embed)
+            embed = Embed(title = f"Results for {search}:", description = found_videos).set_footer(text = "Provided by YouTube")
+            await inter.edit_original_message(content = None, embed = embed)
         except Exception:
+            await inter.edit_original_message("Found Nothing, Please Try Again!")
             traceback.print_exc()
 
     @bot.slash_command(
@@ -138,10 +173,13 @@ def main(token):
         search: str,
         amount: int = 10
     ):
+        await inter.send("Searching...")
         try:
             videos = extras.youtube_search(search, amount)
-            await inter.send("Here's what I found when searching for " + search + ": " + random.choice(videos)["link"])
+            link = random.choice(videos)["link"]
+            await inter.edit_original_message(content = f"Here's what I found when searching for {search}: {link}").set_footer(text = "Provided by YouTube")
         except Exception:
+            await inter.edit_original_message("Found Nothing, Please Try Again!")
             traceback.print_exc()
 
     @bot.slash_command(
@@ -154,19 +192,7 @@ def main(token):
         inter: CommandInteraction,
         question: str
     ):
-        if question.lower().find("gay") != -1:
-            await inter.send("Maybe")
-            return
-        elif question.lower().find("i run") != -1:
-            await inter.send("START RUNNING NOW!")
-            return
-        elif question.lower().find("i die") != -1 or ((question.lower().find("not like") != -1 or question.lower().find("dislike") != -1 or question.lower().find("hate") != -1) and question.lower().find("yoy") != -1) or (question.lower().find("nsfw") != -1 and not (question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1)):
-            await inter.send("No")
-            return
-        elif question.lower().find("not die") != -1 or question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1 or question.lower().find("yoy") != -1:
-            await inter.send("Yes")
-            return
-        await inter.send(speach_types[hash(question.replace(" ", "")) % len(speach_types)].replace("$username", f"<@{inter.author.id}>"))
+        await extras.respond(bot, inter, inter.author, question)
 
     @bot.slash_command(
         description = "This is a mess!",
@@ -227,7 +253,7 @@ def main(token):
 
     @bot.message_command(name = "Base64 Encode")
     async def message_b64_encode(inter: ApplicationCommandInteraction, message: Message):
-        text = message.content[::-1]
+        text = message.content
         try:
             await inter.send(b64.b64encode(text.encode("UTF-8")).decode("UTF-8"))
         except Exception:
@@ -235,36 +261,11 @@ def main(token):
 
     @bot.message_command(name = "Base64 Decode")
     async def message_b64_decode(inter: ApplicationCommandInteraction, message: Message):
-        text = message.content[::-1]
+        text = message.content
         try:
             await inter.send(b64.b64decode(text.encode("UTF-8")).decode("UTF-8"))
         except Exception:
             await inter.send("Cannot encode the text.", ephemeral = True)
-
-    @bot.slash_command(
-        description = "Helps with Research.",
-        options = [
-            Option("search", "Search", OptionType.string, True),
-            Option("amount", "Max Amount", OptionType.integer)
-        ]
-    )
-    async def research(
-        inter: CommandInteraction,
-        search: str,
-        amount: int = 10
-    ):
-        try:
-            searches = googlesearch.search(search, min(amount, 25), advanced = True)
-            found_searches = ""
-            for index, gsearch in enumerate(searches):
-                title = gsearch.title
-                link = gsearch.url
-                found_searches += f"{index + 1}: [{title}]({link})\n"
-            embed = Embed(title = f"Results for {search}:", description = found_searches)
-            await inter.send(embed = embed)
-        except Exception:
-            await inter.send("Found Nothing, Please Try Again!")
-            traceback.print_exc()
 
     @bot.slash_command( 
         description = "Where is he actually?"
@@ -282,22 +283,12 @@ def main(token):
 
         if bot.user.mentioned_in(message) or message.channel.type == ChannelType.private:
             if message.content == f"<@!{bot.user.id}>":
-                await message.channel.send(random.choice(speach_types))
+                speech_type = main.all_speech_types
+                if message.channel.type == ChannelType.private:
+                    speech_type = main.speech_types
+                await message.channel.send(random.choice(speech_type))
             else:
-                question = message.content
-                if question.lower().find("gay") != -1:
-                    await message.channel.send("Maybe")
-                    return
-                elif question.lower().find("i run") != -1:
-                    await message.channel.send("START RUNNING NOW!")
-                    return
-                elif question.lower().find("i die") != -1 or ((question.lower().find("not like") != -1 or question.lower().find("dislike") != -1 or question.lower().find("hate") != -1) and question.lower().find("yoy") != -1) or (question.lower().find("nsfw") != -1 and not (question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1)):
-                    await message.channel.send("No")
-                    return
-                elif question.lower().find("not die") != -1 or question.lower().find("not nsfw") != -1 or question.lower().find("no nsfw") != -1 or question.lower().find("yoy") != -1:
-                    await message.channel.send("Yes")
-                    return
-                await message.channel.send(speach_types[hash(question.replace(f"<@!{bot.user.id}>", "").replace(" ", "")) % len(speach_types)].replace("$username", f"<@!{message.author.id}>"))
+                await extras.respond(bot, message.channel, message.author, message.content, message.channel.type == ChannelType.private)
 
     @bot.event
     async def on_command_error(
